3d01ec33e0183b0613fc7d26b6aaddda7ed8d52a
Eom <yj.um@lifesemantics.kr>
2021-11-04 17:35:50 +0900

    first commit

---
+++ src/main/java/com/example/demo/contoller/IndexControllor.java
@@ -0,0 +1,19 @@
+package com.example.demo.contoller;
+
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexControllor {
+	
+	@GetMapping(value = {"", "/"})
+	public String indexTest(HttpServletRequest request) {
+		System.out.println("±Ù´ö±Ù´ö ¼­¹ö Á×Àº°Å°°Àºµ­");
+		return "index";
+	}
+
+}
